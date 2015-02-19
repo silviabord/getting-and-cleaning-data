c4207d6f0f9ff81e933c1a7a0fa1f3e9e9003aac
Silvia Bordogna <silvia.bordogna0@gmail.com>
2015-02-19 12:42:13 +0100

    final?

--- run_analysis.R
+++ run_analysis.R
@@ -30,21 +30,6 @@ X_train         <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\
 subject_train   <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\subject_train.txt",sep="")))
 y_train         <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\y_train.txt",sep="")))
 
-# files <- list.files(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals",sep=""))
-# 
-# body_acc_x_train        <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\body_acc_x_train.txt",sep="")))
-# body_acc_y_train        <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\body_acc_y_train.txt",sep="")))
-# body_acc_z_train        <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\body_acc_z_train.txt",sep="")))
-# 
-# body_gyro_x_train       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\body_gyro_x_train.txt",sep="")))
-# body_gyro_y_train       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\body_gyro_y_train.txt",sep="")))
-# body_gyro_z_train       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\body_gyro_z_train.txt",sep="")))
-# 
-# total_acc_x_train       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\total_acc_x_train.txt",sep="")))
-# total_acc_y_train       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\total_acc_y_train.txt",sep="")))
-# total_acc_z_train       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\train\\Inertial Signals\\total_acc_z_train.txt",sep="")))
-
-
 #TEST
 
 files <- list.files(paste(zipdir,"\\UCI HAR Dataset\\test",sep=""))
@@ -52,20 +37,6 @@ files <- list.files(paste(zipdir,"\\UCI HAR Dataset\\test",sep=""))
 X_test         <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\X_test.txt",sep="")))
 subject_test   <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\subject_test.txt",sep="")))
 y_test         <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\y_test.txt",sep="")))
-
-# files <- list.files(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals",sep=""))
-# 
-# body_acc_x_test        <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\body_acc_x_test.txt",sep="")))
-# body_acc_y_test        <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\body_acc_y_test.txt",sep="")))
-# body_acc_z_test        <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\body_acc_z_test.txt",sep="")))
-# 
-# body_gyro_x_test       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\body_gyro_x_test.txt",sep="")))
-# body_gyro_y_test       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\body_gyro_y_test.txt",sep="")))
-# body_gyro_z_test       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\body_gyro_z_test.txt",sep="")))
-# 
-# total_acc_x_test       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\total_acc_x_test.txt",sep="")))
-# total_acc_y_test       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\total_acc_y_test.txt",sep="")))
-# total_acc_z_test       <- data.table(read.table(paste(zipdir,"\\UCI HAR Dataset\\test\\Inertial Signals\\total_acc_z_test.txt",sep="")))
 
 
 unlink(zipdir, recursive = T)
@@ -88,24 +59,7 @@ X       <- rbind(X_train,X_test)
 y       <- rbind(y_train,y_test)
 subject <- rbind(subject_train,subject_test)
 
-# body_acc_x <- rbind(body_acc_x_train,body_acc_x_test)
-# body_acc_y <- rbind(body_acc_y_train,body_acc_y_test)
-# body_acc_z <- rbind(body_acc_z_train,body_acc_z_test)
-# 
-# body_gyro_x <- rbind(body_gyro_x_train,body_gyro_x_test)
-# body_gyro_y <- rbind(body_gyro_y_train,body_gyro_y_test)
-# body_gyro_z <- rbind(body_gyro_z_train,body_gyro_z_test)
-# 
-# total_acc_x <- rbind(total_acc_x_train,total_acc_x_test)
-# total_acc_y <- rbind(total_acc_y_train,total_acc_y_test)
-# total_acc_z <- rbind(total_acc_z_train,total_acc_z_test)
-
-rm(list=c("body_acc_x_test","body_acc_x_train","body_acc_y_test",
-          "body_acc_y_train","body_acc_z_test","body_acc_z_train","body_gyro_x_test", 
-          "body_gyro_x_train","body_gyro_y_test","body_gyro_y_train","body_gyro_z_test", 
-          "body_gyro_z_train","subject_test","subject_train","total_acc_x_test",
-          "total_acc_x_train","total_acc_y_test","total_acc_y_train",
-          "total_acc_z_test","total_acc_z_train","X_test","X_train","y_test","y_train"))
+rm(list=c("X_test","X_train","y_test","y_train"))
 
 #2. Appropriately labels the data set with descriptive variable names. 
 features[,names:=gsub("\\(","",V2)]
